73c84be20d2704163c84c3fc6a70ce5b62b45718
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-19 00:56:31 +0530

    top view of binary tree

---
+++ binary-trees/15-top-view-of-binary-tree.cpp
@@ -0,0 +1,42 @@
+class Solution{
+public:
+    vector<int> topView(Node *root){
+        
+        vector<int> res;
+        if(root == NULL)
+            return res;
+
+        // kyuki mujhe har horizontal distance pe ek hi node chaiye to fir horizontal dist ko map kardo node se
+        
+        map < int, int > mpp;
+
+        queue< pair <Node*, int > > q;
+
+        q.push({root, 0});
+
+        while(!q.empty()){
+
+            auto it = q.front();
+            q.pop();
+
+            Node* node = it.first;
+            int horizontal_distance = it.second;
+
+            if(mpp.find(horizontal_distance) == mpp.end())
+                mpp[horizontal_distance] = node->data;
+
+            if(node->left)
+                q.push({node->left, horizontal_distance - 1});
+            if(node->right)
+                q.push({node->right, horizontal_distance + 1});
+        }
+
+        for(auto it : mpp){
+
+            res.push_back(it.second);
+        }
+
+        return res;
+    }
+
+};
