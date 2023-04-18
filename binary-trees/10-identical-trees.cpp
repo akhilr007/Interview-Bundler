9cfe5455928fdbe77bf9370eab9ffb3e6552c734
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-18 23:40:05 +0530

    identical trees

---
+++ binary-trees/10-identical-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL and q == NULL)
+            return true;
+        if(p == NULL or q == NULL)
+            return false;
+
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        bool data = p->val == q->val;
+
+        return left && right && data;
+    }
+};
