3940b76131688fcc7c66f8d0dba94b25ef5fe675
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-19 00:22:19 +0530

    is symmetric tree

---
+++ binary-trees/13-symmetric-binary-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* p, TreeNode* q){
+
+        if(p == NULL and q == NULL)
+            return true;
+        if(p == NULL || q == NULL)
+            return false;
+
+        bool left = isSymmetric(p->left, q->right);
+        bool right = isSymmetric(p->right, q->left);
+        bool data = p->val == q->val;
+
+        return left && right && data;
+    }
+
+    bool isSymmetric(TreeNode* root){
+
+        return isSymmetric(root, root);
+    }
+};
