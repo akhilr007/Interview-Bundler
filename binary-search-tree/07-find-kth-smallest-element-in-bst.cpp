65d3af4bc701c3e8872c4b44b77eeb002fc2d022
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-22 15:07:52 +0530

    find kth smallest in bst

---
+++ binary-search-tree/07-find-kth-smallest-element-in-bst.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // inorder traversal in bst always gives sorted array
+    void inorder(TreeNode* root, int& k, int& ans){
+
+        if(root == NULL)
+            return;
+
+        inorder(root->left, k, ans);
+
+        k--;
+        if(k == 0)
+        {
+            ans = root->val;
+            return;
+        }
+
+        inorder(root->right, k, ans);
+    }
+    int kthSmallest(TreeNode* root, int k){
+
+        int ans = 0;
+        inorder(root, k, ans);
+        return ans;
+    }
+}
