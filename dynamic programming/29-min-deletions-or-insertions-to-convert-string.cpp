a181d9bce488135926c105bb0b07ee111a107592
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-30 21:42:25 +0530

    min insertion or deletions to convert string

--- dynamic programming/29-min-deletions-or-insertions-to-convert-string.cpp
+++ dynamic programming/29-min-deletions-or-insertions-to-convert-string.cpp
@@ -1,39 +1,38 @@
-/*
- s1 = "leetcode", s2="etco"
+/**
+s1 = "leetcode", s2="etco"
 
- to make both string equal 
-
- 1. find longest common subsequence b/w them
- 2. no ans will be n-lcs + m-lcs
+1. find lcs , lcs = etco 
+2. n - lcs + m - lcs
 */
 
 class Solution{
 public:
     int findLCS(string s1, string s2){
 
-        int n = s.size();
+        int n = s1.size();
+        int m = s2.size();
 
-        vector<int> prev(n+1, 0), cur(n+1, 0);
+        vector<int> prev(m+1, 0), cur(m+1, 0);
 
         for(int i=1; i<=n; i++){
-
-            for(int j=1; j<=n; j++){
+            for(int j=1; j<=m; j++){
 
                 int ans=0;
-                if(s[i-1] == r[j-1])
+                if(s1[i-1] == s2[j-1])
                     ans = 1 + prev[j-1];
                 else
                     ans = max(prev[j], cur[j-1]);
 
                 cur[j] = ans;
             }
+
             prev = cur;
         }
 
-        return prev[n];
+        return prev[m];
     }
-    
-    int minDeletion(string s1, string s2){
+
+    int minDeletion(string s1 , string s2){
 
         int n = s1.size();
         int m = s2.size();
